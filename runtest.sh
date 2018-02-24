53ff9b25e356fd2196c0adee0c335743d336a7e2
Aske Simon Christensen <askesc@gmail.com>
2018-02-24 18:41:35 +0100

    Update test script

--- runtest.sh
+++ runtest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for mod in examples/*.mod; do
 	M1=${mod/examples/test}
-	M2=${M1/.mod/.cinter3}
+	M2=${M1/.mod/.cinter4}
 	M3=${M1/.mod/.out}
-	./ProtrackerConvert.py $mod $M2 >$M3
-	echo `tail -1 $M3` `md5sum $M2`
+	convert/CinterConvert.py $mod $M2 >$M3
+	echo `grep "[ ]errors[.]" $M3` `md5sum $M2`
 done
